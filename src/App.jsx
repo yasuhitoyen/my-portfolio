84b2bfa7900c5081398efe247b58802c1cca81a4
Michael Yen <myyen@wisc.edu>
2025-06-04 15:01:53 -0700

    Changed main bg color

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { motion, useScroll } from "framer-motion";
 
 const App = () => {
   return (
-    <div className=" w-full h-[4000px] c1">
+    <div className=" w-full h-[4000px] bg-[#F8F5EF]">
       <Home></Home>
       
     </div>
